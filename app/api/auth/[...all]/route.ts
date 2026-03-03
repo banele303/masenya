284867c561c0b81e7dc6fa7b56b5dc6200de571c
alex banele <alexsouthflow@gmail.com>
2026-03-03 04:47:05 +0200

    we there

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -3,4 +3,19 @@ export const dynamic = "force-dynamic";
 import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
-export const { GET, POST } = toNextJsHandler(auth);
+let handler: { GET: Function; POST: Function } | null = null;
+
+function getHandler() {
+  if (!handler) {
+    handler = toNextJsHandler(auth);
+  }
+  return handler;
+}
+
+export async function GET(req: Request) {
+  return getHandler()!.GET(req);
+}
+
+export async function POST(req: Request) {
+  return getHandler()!.POST(req);
+}
